09487924d738e8105689e720ebb29cdfd6b66a4c
Karel Picman <karel.picman@kontron.com>
2018-02-01 15:07:13 +0100

    Revert "Adding executable property'"
    This reverts commit 9cba2f83ba335f81d1c2a4c615edf5f073438f42.

--- lib/dav4rack/resource.rb
+++ lib/dav4rack/resource.rb
@@ -375,7 +375,7 @@ module DAV4Rack
       getlastmodified
       creationdate
       displayname
-      executable
+      execute
     ).map{|prop| { name: prop, ns_href: DAV_NAMESPACE } }.freeze
 
     def properties
