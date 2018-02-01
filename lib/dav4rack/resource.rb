3a7420b25caf61d2ebe0241f280c8a70f50eac7f
Karel Picman <karel.picman@kontron.com>
2018-02-01 14:22:57 +0100

    Adding execute property'

--- lib/dav4rack/resource.rb
+++ lib/dav4rack/resource.rb
@@ -375,6 +375,7 @@ module DAV4Rack
       getlastmodified
       creationdate
       displayname
+      execute
     ).map{|prop| { name: prop, ns_href: DAV_NAMESPACE } }.freeze
 
     def properties
